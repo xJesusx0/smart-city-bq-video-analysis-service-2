7ddd119b3da0344dcf9d946566911ead1b248d00
xJesusx0 <snowcatyt1234@gmail.com>
2026-01-25 13:27:08 -0500

    feat: agregar lectura de video y detección

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
 import cv2
 from ultralytics import YOLO
 
-
 yolo_model = 'yolo26n.pt'
+classes = {
+    0: "person",
+    1: "bicycle",
+    2: "car",
+    3: "motorcycle",
+    5: "bus",
+    7: "truck",
+    9: "traffic light",
+    11: "stop sign"    
+}
+
+classes_id = [ key for key, value in classes.items() ]
+
 
 model = YOLO(yolo_model)
-print(yolo_model)
+model.task = 'detect'
+model.conf = 0.3
+cap = cv2.VideoCapture('videos/prueba.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, stream=True, classes = classes_id)
+    for result in results: 
+        for box in result.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = float(box.conf[0])
+            cls = int(box.cls[0])
+            label = model.names[cls] 
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, f'{label} {conf:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+
+    cv2.imshow("YOLO Video", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
